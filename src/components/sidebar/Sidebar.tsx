ba2522dc43341d238ae251d93270f8483f9e74bf
ImamIsmailTambili <71882400+ImamIsmailTambili@users.noreply.github.com>
2026-01-18 18:49:02 +0700

    Update Sidebar.tsx

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
         <aside className="w-10 md:w-50 lg:w-64 h-full bg-white border-r border-slate-200 flex flex-col">
             {/* Logo */}
             <div className="p-5 text-center">
-                <Link href="/Dashboard" className="text-3xl font-logo text-blue-950">
+                <Link href="/" className="text-3xl font-logo text-blue-950">
                     Cinemain
                 </Link>
             </div>
